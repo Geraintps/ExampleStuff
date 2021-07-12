df38ed97bda6cad51aabfa96ecd8a96c384ac479
Geraint Pickard-Skeats <geraintps@gmail.com>
2021-07-12 17:47:43 +0100

    Updates

--- ThePebbles/scripts/nav.js
+++ ThePebbles/scripts/nav.js
@@ -7,11 +7,11 @@ function menu() {
   var body = document.body;
 
 
-  var burgerNav = document.getElementsByClassName('b-nav')[0];
+  var nav = document.getElementsByClassName('nav')[0];
   var logo = document.getElementsByClassName("logo")[0];
  
 
-  [body, burgerNav, logo].forEach(function (el) {
+  [body, nav, logo].forEach(function (el) {
     el.classList.toggle('open');
   });
 
